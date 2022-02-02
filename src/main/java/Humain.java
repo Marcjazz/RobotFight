ffaa375cfecdc1ae09a27ba3bc75e793ff9c935e
Marco Kuidja <kuidjamarco@gmail.com>
2022-02-02 09:33:32 +0100

    Test du tire d'un humain qui peut soit rater ou cibler

---
+++ src/main/java/Humain.java
@@ -0,0 +1,36 @@
+public class Humain {
+    private String nom;
+    private int lifePoints;
+
+    public Humain(String nom) {
+        this.nom = nom;
+        this.lifePoints = 10;
+    }
+
+    public void fire(Robot robot) {
+        double random = Math.random()*10;
+        if(random > 5) robot.setLifePoints(robot.getLifePoints() - 2);
+        else System.out.println("l'humain a raté sa cible");
+    }
+    public void fire(Humain humain) {
+        double random = Math.random()*10;
+        if(random > 5) humain.setLifePoints(humain.getLifePoints() - 2);
+        else System.out.println("l'humain a raté sa cible");
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getLifePoints() {
+        return lifePoints;
+    }
+
+    public void setLifePoints(int lifePoints) {
+        this.lifePoints = lifePoints;
+    }
+}
---
+++ src/test/java/HumainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HumainTest {
+    Robot robot1 = new Robot("D2R2");
+    Humain humain = new Humain("Marco");
+
+    @Test
+    void fire() {
+        int lifePointBefore =  robot1.getLifePoints();
+        humain.fire(robot1);
+        int lifePointAfter = robot1.getLifePoints();
+        assertEquals(lifePointBefore, lifePointAfter);
+    }
+}
