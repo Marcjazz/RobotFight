4ad513980ebc5af35b7e74a14ce5019fc394f66e
@julie_Michi <julie.rapidos@gmail.com>
2022-02-02 06:12:01 +0100

    ce bout de code permet de savoir si un robot est mort

--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -1,12 +1,22 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
-
+    Robot robot1 = new Robot("D2R2");
+    Robot robot2 = new Robot("Data");
     @org.junit.jupiter.api.Test
     void fire() {
     }
 
     @org.junit.jupiter.api.Test
-    void isDead() {
+    void TestisDead() {
+        robot1.fire(robot2);
+        robot1.fire(robot2);
+        robot1.fire(robot2);
+        robot1.fire(robot2);
+        robot1.fire(robot2);
+
+        assertFalse(robot1.isDead());
+        assertTrue(robot2.isDead());
     }
+
 }
