7bf485f53806b9b7210f6ee98d4e7f7fe7119cd8
Stephane Nouboudem <Stephane Nouboudem>
2022-02-02 09:58:25 +0100

    test de tir d'un robot sur un autre

--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -2,8 +2,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
 
+    Robot rb1 = new Robot("d2r2");
+    Robot rb2 = new Robot("data");
+
+
     @org.junit.jupiter.api.Test
     void fire() {
+        int lifebeforeCombat = rb2.getLifePoints();
+        rb1.fire(rb2);
+        int lifeafterCombat = rb2.getLifePoints();
+        System.out.println(rb1.getLifePoints());
+        System.out.println(rb2.getLifePoints());
+        assertTrue(lifeafterCombat<lifebeforeCombat);
     }
 
     @org.junit.jupiter.api.Test
