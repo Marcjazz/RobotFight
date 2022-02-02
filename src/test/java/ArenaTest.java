eb0f0715983a567c634d4e2a2ccbfd97eb64569c
Ives <iveschatelin@gmail.com>
2022-02-02 10:11:09 +0100

    test qui verifie le combat entre robots

--- src/test/java/ArenaTest.java
+++ src/test/java/ArenaTest.java
@@ -2,8 +2,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ArenaTest {
 
+    Robot rb1 = new Robot("D1R1");
+    Robot rb2 = new Robot("D2R2");
+    Arena ar = new Arena();
+
     @org.junit.jupiter.api.Test
     void fight() {
-        Robot robot1 = new Robot("D2R2");
+        ar.fight(rb1, rb2);
+        assertTrue((rb1.getLifePoints()==0) || (rb2.getLifePoints()==0));
     }
 }
--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -2,8 +2,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
 
+
     @org.junit.jupiter.api.Test
     void fire() {
+
     }
 
     @org.junit.jupiter.api.Test
